e663c0bdfaf91aa3875007657e434425cb590d21
Aditya Solanki <56908240+adityasolanki205@users.noreply.github.com>
2021-03-09 23:47:49 +0530

    Create publish_to_pubsub-checkpoint.py

---
+++ .ipynb_checkpoints/publish_to_pubsub-checkpoint.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from generating_data import generate_log
+import logging
+from google.cloud import pubsub_v1
+import random
+import time
+
+
+
+PROJECT_ID="trusty-field-283517"
+TOPIC = "german_credit_data"
+
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path(PROJECT_ID, TOPIC)
+
+
+def publish(publisher, topic_path, message):
+    data = message.encode('utf-8')
+    return publisher.publish(topic_path, data = data)
+
+def callback(message_future):
+    if message_future.exception(timeout=30):
+        print('Publishing message on {} threw an Exception {}.'.format(
+            topic_name, message_future.exception()))
+    else:
+        print(message_future.result())
+
+if __name__ == '__main__':
+
+    while True:
+        line = generate_log()
+        print(line)
+        message_future = publish(publisher, topic_path, line)
+        message_future.add_done_callback(callback)
+
+        sleep_time = random.choice(range(1, 3, 1))
+        time.sleep(sleep_time)
+
