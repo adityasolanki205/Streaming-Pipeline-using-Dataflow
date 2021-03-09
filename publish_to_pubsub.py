5e02946972a832aac2c7ab8dde23cfb6ea50982f
Aditya Solanki <56908240+adityasolanki205@users.noreply.github.com>
2021-03-09 23:47:54 +0530

    Update publish_to_pubsub.py

--- publish_to_pubsub.py
+++ publish_to_pubsub.py
@@ -11,8 +11,6 @@ import random
 import time
 
 
-# In[ ]:
-
 
 PROJECT_ID="trusty-field-283517"
 TOPIC = "german_credit_data"
@@ -21,12 +19,16 @@ publisher = pubsub_v1.PublisherClient()
 topic_path = publisher.topic_path(PROJECT_ID, TOPIC)
 
 
-# In[ ]:
-
-
 def publish(publisher, topic_path, message):
     data = message.encode('utf-8')
     return publisher.publish(topic_path, data = data)
+
+def callback(message_future):
+    if message_future.exception(timeout=30):
+        print('Publishing message on {} threw an Exception {}.'.format(
+            topic_name, message_future.exception()))
+    else:
+        print(message_future.result())
 
 if __name__ == '__main__':
 
@@ -34,7 +36,7 @@ if __name__ == '__main__':
         line = generate_log()
         print(line)
         message_future = publish(publisher, topic_path, line)
-        #message_future.add_done_callback(callback)
+        message_future.add_done_callback(callback)
 
         sleep_time = random.choice(range(1, 3, 1))
         time.sleep(sleep_time)
